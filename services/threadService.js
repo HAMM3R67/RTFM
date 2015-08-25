4d7840b2d3fd762321edca79ab4dcd30ea2f35e2
HAMM3R67 <tcschauer@gmail.com>
2015-08-25 05:14:12 -0600

    removed hard coded data from service, firebase functioning

--- services/threadService.js
+++ services/threadService.js
@@ -1,33 +1,5 @@
 angular.module("rtfmApp").service("threadService", function($firebaseArray, $firebaseObject, fb){
-    var data = {
-        "threads": [
-            {
-                "title": "A new thread!",
-                "username": "Scott",                
-                "comments": [
-                    {
-                        "text": "This is the best title I have ever seen for a thread",
-                        "username": "Phil"
-                    },
-                    {
-                        "text": "I know right?",
-                        "username": "Joe Somebody"
-                    }
-                ]
-            },
-            {
-                "title": "Another thread",
-                "username": "Joe Somebody",
-                "comments": []
-            },
-            {
-                "title": "Yet another thread",
-                "username": "Phil",
-                "comments": []
-            }
-        ]
-    }    
-
+  
     this.getAllThreads = function(){
         var ref = new Firebase(fb.url + '/threads/') 
         return $firebaseArray(ref);

8b06979ece4e057c33ec1d909c51292301094c89
HAMM3R67 <tcschauer@gmail.com>
2015-08-25 05:34:08 -0600

    used the bindTo method to create a 3 way binding for $scope.thread

--- pages/thread/thread.js
+++ pages/thread/thread.js
@@ -3,7 +3,9 @@ angular.module('rtfmApp')
     var threadId = parseInt($routeParams.threadId);
 
     $scope.newComment = '';
-    $scope.thread = threadService.getThread(threadId);
+    var thread = threadService.getThread(threadId);
+
+    thread.bindTo($scope, 'thread')  //creates a $scope.thread with 3-way binding
 
     $scope.addComment= function(){ 
         if(!$scope.newComment){
